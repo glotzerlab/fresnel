4fac951a6785ad84664cfe2c35ba953978d477ea
Joshua A. Anderson <joaander@umich.edu>
2016-08-25 08:22:17 -0400

    Add OptiX credits to the credits doc page
    Also enable sphinx docs to build again, including a placeholder _common
    library, and changing a few names over to Fresnel from an earlier name
    assigned to this repository.
    
    refs #1

---
+++ fresnel/_common.py
@@ -0,0 +1,17 @@
+# placeholder to take place of _common.so when building sphinx docs from source
+
+class Material:
+    def __init__(self, *args, **kwargs):
+        pass
+
+class RGBf:
+    def __init__(self, *args, **kwargs):
+        pass
+
+class Camera:
+    def __init__(self, *args, **kwargs):
+        pass
+
+class vec3f:
+    def __init__(self, *args, **kwargs):
+        pass
