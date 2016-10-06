744be0b0c6a49e622e35635f244d4ec9797bb8b5
Vyas <vramasub@umich.edu>
2016-10-06 19:19:34 -0400

    Added sphere geometry to all of the necessary files

--- fresnel/gpu/module-gpu.cc
+++ fresnel/gpu/module-gpu.cc
@@ -5,6 +5,7 @@
 #include "Device.h"
 #include "Scene.h"
 #include "Geometry.h"
+#include "GeometrySphere.h"
 // #include "GeometryTriangleMesh.h"
 #include "Tracer.h"
 #include "TracerWhitted.h"
@@ -28,6 +29,7 @@ PYBIND11_PLUGIN(_gpu)
     export_Device(m);
     export_Scene(m);
     export_Geometry(m);
+    export_GeometrySphere(m);
     // export_GeometryTriangleMesh(m);
     export_Tracer(m);
     export_TracerWhitted(m);
