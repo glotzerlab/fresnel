ba4009bae50982ca8c2b1524ecfeefadb8d5b5ce
Joshua A. Anderson <joaander@umich.edu>
2018-02-21 13:46:24 -0500

    Fix duplicate ID

--- fresnel/gpu/TracerIDs.h
+++ fresnel/gpu/TracerIDs.h
@@ -1,7 +1,7 @@
 #ifndef __TRACER_RAY_IDS_H__
 #define __TRACER_RAY_IDS_H__
 
-const unsigned int TRACER_PREVIEW_RAY_ID = 1;
+const unsigned int TRACER_PREVIEW_RAY_ID = 0;
 const unsigned int TRACER_PATH_RAY_ID = 1;
 
 #endif
