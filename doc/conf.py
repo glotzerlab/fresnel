cceccd7423a37702607c9e68f289278aa5537b29
Joshua A. Anderson <joaander@umich.edu>
2019-03-01 16:00:44 -0500

    Add umich/glotzer branding to doc footer

--- doc/conf.py
+++ doc/conf.py
@@ -142,7 +142,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -177,7 +177,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
