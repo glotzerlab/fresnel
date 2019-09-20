97029a4e4b280647048c5ea10b011e2851c4ecc2
Joshua A. Anderson <joaander@umich.edu>
2019-09-20 14:18:26 -0400

    Embed both qhull_r and qhullcpp if qhullcpp is broken

--- CMake/FindQhull.cmake
+++ CMake/FindQhull.cmake
@@ -13,14 +13,7 @@ find_package_handle_standard_args(Qhull
                                   REQUIRED_VARS Qhull_r_LIBRARY Qhull_INCLUDE_DIR)
 
 
-if(Qhull_r_LIBRARY AND NOT TARGET QHull::qhull_r)
-    add_library(QHull::qhull_r UNKNOWN IMPORTED)
-    set_target_properties(QHull::qhull_r PROPERTIES
-        IMPORTED_LOCATION "${Qhull_r_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
-endif()
-
-if(Qhull_cpp_LIBRARY)
+if(Qhull_r_LIBRARY AND Qhull_cpp_LIBRARY)
     set(CMAKE_REQUIRED_LIBRARIES "${Qhull_cpp_LIBRARY}; ${Qhull_r_LIBRARY}")
     set(CMAKE_REQUIRED_INCLUDES "${Qhull_INCLUDE_DIR}")
     check_cxx_source_compiles("#include \"libqhullcpp/Qhull.h\"\nint main(int argc, char **argv) { orgQhull::Qhull q; return 0;}" can_link_libqhullcpp)
@@ -31,12 +24,38 @@ if(Qhull_cpp_LIBRARY)
             IMPORTED_LOCATION "${Qhull_cpp_LIBRARY}"
             INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
     endif()
+
+    if(can_link_libqhullcpp AND NOT TARGET QHull::qhull_r)
+        add_library(QHull::qhull_r UNKNOWN IMPORTED)
+        set_target_properties(QHull::qhull_r PROPERTIES
+            IMPORTED_LOCATION "${Qhull_r_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
+    endif()
 else()
     set(can_link_libqhullcpp "FALSE")
 endif()
 
 # build libqhullcpp if needed
 if(NOT can_link_libqhullcpp)
+    set(
+        libqhullr_SOURCES
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/global_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/stat_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/geom2_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/poly2_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/merge_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/libqhull_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/geom_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/poly_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/qset_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/mem_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/random_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/usermem_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/io_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/user_r.c
+            ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhull_r/rboxlib_r.c
+    )
+
     set(libqhullcpp_SOURCES
         ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/Coordinates.cpp
         ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/PointCoordinates.cpp
@@ -61,6 +80,13 @@ if(NOT can_link_libqhullcpp)
 
     add_library(qhull_cpp STATIC ${libqhullcpp_SOURCES})
     set_target_properties(qhull_cpp PROPERTIES INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/extern/qhull/src)
+    set_target_properties(qhull_cpp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/extern/qhull/src)
     set_target_properties(qhull_cpp PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
     add_library(QHull::qhull_cpp ALIAS qhull_cpp)
+
+    add_library(qhull_r STATIC ${libqhullr_SOURCES})
+    set_target_properties(qhull_r PROPERTIES INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/extern/qhull/src)
+    set_target_properties(qhull_r PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/extern/qhull/src)
+    set_target_properties(qhull_r PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+    add_library(QHull::qhull_r ALIAS qhull_r)
 endif()
