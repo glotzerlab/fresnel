ae637ce06c80d7f6b6fe6057ea895d76f1107039
Joshua A. Anderson <joaander@umich.edu>
2019-09-20 09:18:29 -0400

    Build libqhullcpp if it is missing or broken

--- CMake/FindQhull.cmake
+++ CMake/FindQhull.cmake
@@ -10,7 +10,7 @@ find_library(Qhull_cpp_LIBRARY qhullcpp
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Qhull
-                                  REQUIRED_VARS Qhull_r_LIBRARY Qhull_cpp_LIBRARY Qhull_INCLUDE_DIR)
+                                  REQUIRED_VARS Qhull_r_LIBRARY Qhull_INCLUDE_DIR)
 
 
 if(Qhull_r_LIBRARY AND NOT TARGET QHull::qhull_r)
@@ -20,9 +20,47 @@ if(Qhull_r_LIBRARY AND NOT TARGET QHull::qhull_r)
         INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
 endif()
 
-if(Qhull_cpp_LIBRARY AND NOT TARGET QHull::qhull_cpp)
-    add_library(QHull::qhull_cpp UNKNOWN IMPORTED)
-    set_target_properties(QHull::qhull_cpp PROPERTIES
-        IMPORTED_LOCATION "${Qhull_cpp_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
+if(Qhull_cpp_LIBRARY)
+    set(CMAKE_REQUIRED_LIBRARIES "${Qhull_cpp_LIBRARY}")
+    set(CMAKE_REQUIRED_INCLUDES "${Qhull_INCLUDE_DIR}")
+    check_cxx_source_compiles("#include \"libqhullcpp/Qhull.h\"; int main(int argc, char **argv) { orgQhull::Qhull q; return 0;}" can_link_libqhullcpp)
+
+    if(can_link_libqhullcpp AND NOT TARGET QHull::qhull_cpp)
+        add_library(QHull::qhull_cpp UNKNOWN IMPORTED)
+        set_target_properties(QHull::qhull_cpp PROPERTIES
+            IMPORTED_LOCATION "${Qhull_cpp_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${Qhull_INCLUDE_DIR}")
+    endif()
+else()
+    set(can_link_libqhullcpp "FALSE")
+endif()
+
+# build libqhullcpp if needed
+if(NOT can_link_libqhullcpp)
+    set(libqhullcpp_SOURCES
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/Coordinates.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/PointCoordinates.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/Qhull.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullFacet.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullFacetList.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullFacetSet.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullHyperplane.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullPoint.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullPointSet.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullPoints.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullQh.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullRidge.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullSet.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullStat.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullVertex.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/QhullVertexSet.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/RboxPoints.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/RoadError.cpp
+        ${CMAKE_SOURCE_DIR}/extern/qhull/src/libqhullcpp/RoadLogEvent.cpp
+        )
+
+    add_library(qhull_cpp OBJECT ${libqhullcpp_SOURCES})
+    set_target_properties(qhull_cpp PROPERTIES INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/extern/qhull/src)
+    set_target_properties(qhull_cpp PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+    add_library(QHull::qhull_cpp ALIAS qhull_cpp)
 endif()
