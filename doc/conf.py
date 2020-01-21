6c7854eeefe9db5a8611a3712ea87dc6c431506c
Joshua A. Anderson <joaander@umich.edu>
2020-01-21 08:29:44 -0500

    Update copyright

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'fresnel'
-copyright = '2016-2019, The Regents of the University of Michigan'
+copyright = '2016-2020, The Regents of the University of Michigan'
 author = 'The Regents of the University of Michigan'
 
 # The version info for the project you're documenting, acts as replacement for
--- fresnel/common/RayGen.h
+++ fresnel/common/RayGen.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2019 The Regents of the University of Michigan
+// Copyright (c) 2016-2020 The Regents of the University of Michigan
 // This file is part of the Fresnel project, released under the BSD 3-Clause License.
 
 #include "Random123/philox.h"
