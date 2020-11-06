25fffd6d97547fbbca0baa602532f79412e74fc4
Corwin Kerr <cbkerr@umich.edu>
2020-11-05 21:58:40 -0500

    Intuitive changes to GeometryEllipsoid.cc

--- fresnel/cpu/GeometryEllipsoid.cc
+++ fresnel/cpu/GeometryEllipsoid.cc
@@ -4,19 +4,19 @@
 #include <pybind11/stl.h>
 #include <stdexcept>
 
-#include "GeometrySphere.h"
-#include "common/IntersectSphere.h"
+#include "GeometryEllipsoid.h"
+#include "common/IntersectEllipsoid.h"
 
 namespace fresnel
     {
 namespace cpu
     {
 /*! \param scene Scene to attach the Geometry to
-    \param N number of spheres to manage
+    \param N number of ellipsoids to manage
 
-    Initialize the sphere geometry.
+    Initialize the ellipsoid geometry.
 */
-GeometrySphere::GeometrySphere(std::shared_ptr<Scene> scene, unsigned int N) : Geometry(scene)
+GeometryEllipsoid::GeometryEllipsoid(std::shared_ptr<Scene> scene, unsigned int N) : Geometry(scene)
     {
     // create the geometry
     m_geometry = rtcNewGeometry(m_device->getRTCDevice(), RTC_GEOMETRY_TYPE_USER);
@@ -32,15 +32,16 @@ GeometrySphere::GeometrySphere(std::shared_ptr<Scene> scene, unsigned int N) : G
 
     // initialize the buffers
     m_position = std::shared_ptr<Array<vec3<float>>>(new Array<vec3<float>>(N));
-    m_radius = std::shared_ptr<Array<float>>(new Array<float>(N));
+    m_radii = std::shared_ptr<Array<vec3<float>>>(new Array<vec3<float>>(N));
+	m_orientation = std::shared_ptr<Array<quat<float>>>(new Array<quat<float>>);
     m_color = std::shared_ptr<Array<RGB<float>>>(new Array<RGB<float>>(N));
 
     // register functions for embree
     rtcSetGeometryUserData(m_geometry, this);
     m_device->checkError();
-    rtcSetGeometryBoundsFunction(m_geometry, &GeometrySphere::bounds, NULL);
+    rtcSetGeometryBoundsFunction(m_geometry, &GeometryEllipsoid::bounds, NULL);
     m_device->checkError();
-    rtcSetGeometryIntersectFunction(m_geometry, &GeometrySphere::intersect);
+    rtcSetGeometryIntersectFunction(m_geometry, &GeometryEllipsoid::intersect);
     m_device->checkError();
 
     rtcCommitGeometry(m_geometry);
@@ -49,52 +50,58 @@ GeometrySphere::GeometrySphere(std::shared_ptr<Scene> scene, unsigned int N) : G
     m_valid = true;
     }
 
-GeometrySphere::~GeometrySphere() { }
+GeometryEllipsoid::~GeometryEllipsoid() { }
 
 /*! Compute the bounding box of a given primitive
 
-    \param ptr Pointer to a GeometrySphere instance
+    \param ptr Pointer to a GeometryEllipsoid instance
     \param item Index of the primitive to compute the bounding box of
     \param bounds_o Output bounding box
 */
-void GeometrySphere::bounds(const struct RTCBoundsFunctionArguments* args)
+void GeometryEllipsoid::bounds(const struct RTCBoundsFunctionArguments* args)
     {
-    GeometrySphere* geom = (GeometrySphere*)args->geometryUserPtr;
+    GeometryEllipsoid* geom = (GeometryEllipsoid*)args->geometryUserPtr;
     vec3<float> p = geom->m_position->get(args->primID);
-    float radius = geom->m_radius->get(args->primID);
-    RTCBounds& bounds_o = *args->bounds_o;
-    bounds_o.lower_x = p.x - radius;
-    bounds_o.lower_y = p.y - radius;
-    bounds_o.lower_z = p.z - radius;
+	vec3<float> radii = geom->m_radii->get(args->primID);
 
-    bounds_o.upper_x = p.x + radius;
-    bounds_o.upper_y = p.y + radius;
-    bounds_o.upper_z = p.z + radius;
+	// Use a too-big box for now
+    float max_radius = std::max_element(radii.begin(), radii.end());
+    RTCBounds& bounds_o = *args->bounds_o;
+    bounds_o.lower_x = p.x - max_radius;
+    bounds_o.lower_y = p.y - max_radius;
+    bounds_o.lower_z = p.z - max_radius;
+
+    bounds_o.upper_x = p.x + max_radius;
+    bounds_o.upper_y = p.y + max_radius;
+    bounds_o.upper_z = p.z + max_radius;
     }
 
 /*! Compute the intersection of a ray with the given primitive
 
-    \param ptr Pointer to a GeometrySphere instance
+    \param ptr Pointer to a GeometryEllipsoid instance
     \param ray The ray to intersect
     \param item Index of the primitive to compute the bounding box of
 */
-void GeometrySphere::intersect(const struct RTCIntersectFunctionNArguments* args)
+void GeometryEllipsoid::intersect(const struct RTCIntersectFunctionNArguments* args)
     {
-    GeometrySphere* geom = (GeometrySphere*)args->geometryUserPtr;
+    GeometryEllipsoid* geom = (GeometryEllipsoid*)args->geometryUserPtr;
     const vec3<float> position = geom->m_position->get(args->primID);
-    const float radius = geom->m_radius->get(args->primID);
+	vec3<float> radii = geom->m_radii->get(args->primID);
+	quat<float> orientation = geom->m_orientation(args->primID)
     RTCRayHit& rayhit = *(RTCRayHit*)args->rayhit;
     RTCRay& ray = rayhit.ray;
 
     float t = 0, d = 0;
     vec3<float> N;
-    bool hit = intersect_ray_sphere(t,
-                                    d,
-                                    N,
-                                    vec3<float>(ray.org_x, ray.org_y, ray.org_z),
-                                    vec3<float>(ray.dir_x, ray.dir_y, ray.dir_z),
-                                    position,
-                                    radius);
+    bool hit = intersect_ray_ellipsoid(t,
+									   d,
+									   N,
+									   vec3<float>(ray.org_x, ray.org_y, ray.org_z),
+									   vec3<float>(ray.dir_x, ray.dir_y, ray.dir_z),
+									   position,
+									   radii,
+									   orientation
+									  );
 
     if (hit && (ray.tnear < t) && (t < ray.tfar))
         {
@@ -115,13 +122,14 @@ void GeometrySphere::intersect(const struct RTCIntersectFunctionNArguments* args
 
 /*! \param m Python module to export in
  */
-void export_GeometrySphere(pybind11::module& m)
+void export_GeometryEllipsoid(pybind11::module& m)
     {
-    pybind11::class_<GeometrySphere, Geometry, std::shared_ptr<GeometrySphere>>(m, "GeometrySphere")
+    pybind11::class_<GeometryEllipsoid, Geometry, std::shared_ptr<GeometryEllipsoid>>(m, "GeometryEllipsoid")
         .def(pybind11::init<std::shared_ptr<Scene>, unsigned int>())
-        .def("getPositionBuffer", &GeometrySphere::getPositionBuffer)
-        .def("getRadiusBuffer", &GeometrySphere::getRadiusBuffer)
-        .def("getColorBuffer", &GeometrySphere::getColorBuffer);
+        .def("getPositionBuffer", &GeometryEllipsoid::getPositionBuffer)
+        .def("getRadiiBuffer", &GeometryEllipsoid::getRadiiBuffer)
+		.def("getOrientationBuffer", &GeometryEllipsoid::getOrientationBuffer)
+        .def("getColorBuffer", &GeometryEllipsoid::getColorBuffer);
     }
 
     } // namespace cpu
