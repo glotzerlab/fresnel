f00f146891811f77bc38959cad03b7e7375b087a
Joshua A. Anderson <joaander@umich.edu>
2020-11-12 11:57:35 -0500

    Fix gallery scripts to use new API

--- doc/gallery/sphere.py
+++ doc/gallery/sphere.py
@@ -3,6 +3,7 @@
 import fresnel
 import numpy
 import PIL
+import sys
 
 data = numpy.load('spheres.npz')
 
@@ -19,8 +20,10 @@ geometry.material = fresnel.material.Material(color=fresnel.color.linear(
                                               roughness=0.8,
                                               specular=0.2)
 
+scene.camera = fresnel.camera.Orthographic.fit(scene)
 out = fresnel.pathtrace(scene, samples=64, light_samples=32, w=580, h=580)
 PIL.Image.fromarray(out[:], mode='RGBA').save('sphere.png')
 
-out = fresnel.pathtrace(scene, samples=256, light_samples=16, w=1380, h=1380)
-PIL.Image.fromarray(out[:], mode='RGBA').save('sphere-hires.png')
+if len(sys.argv) > 1 and sys.argv[1] == 'hires':
+    out = fresnel.pathtrace(scene, samples=256, light_samples=16, w=1380, h=1380)
+    PIL.Image.fromarray(out[:], mode='RGBA').save('sphere-hires.png')
