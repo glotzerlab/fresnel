03705e706d1afae5b6f6e0b979f1c9e9b5f38a29
Joshua A. Anderson <joaander@umich.edu>
2021-05-06 11:14:12 -0400

    Render workflows in pre-commit

--- .github/workflows/make_workflows.py
+++ .github/workflows/make_workflows.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
+"""Generate GitHub Actions workflows from the templates."""
+
 import jinja2
 import yaml
 import os
+
+optix_docker_options = "--mount type=bind,source=/usr/lib/libnvidia-rtcore.so,"\
+                       "target=/usr/lib/libnvidia-rtcore.so --mount type=bind,"\
+                       "source=/usr/lib/libnvoptix.so,"\
+                       "target=/usr/lib/libnvoptix.so --gpus=all"
 
 if __name__ == '__main__':
     # change to the directory of the script
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'),
-                             block_start_string = '<%',
-                             block_end_string = '%>',
-                             variable_start_string = '<<',
-                             variable_end_string = '>>',
-                             comment_start_string = '<#',
-                             comment_end_string = '#>',
+                             block_start_string='<%',
+                             block_end_string='%>',
+                             variable_start_string='<<',
+                             variable_end_string='>>',
+                             comment_start_string='<#',
+                             comment_end_string='#>',
                              trim_blocks=True,
                              lstrip_blocks=True)
     template = env.get_template('unit_test.yml')
@@ -23,11 +30,11 @@ if __name__ == '__main__':
 
     # preprocess configurations and fill out additional fields needed by
     # `unit_test.yml` to configure the matrix jobs
-    for name,configuration in configurations.items():
+    for name, configuration in configurations.items():
         for entry in configuration:
             if entry['config'].startswith('[optix'):
                 entry['runner'] = "[self-hosted,GPU]"
-                entry['docker_options'] = "--mount type=bind,source=/usr/lib/libnvidia-rtcore.so,target=/usr/lib/libnvidia-rtcore.so --mount type=bind,source=/usr/lib/libnvoptix.so,target=/usr/lib/libnvoptix.so --gpus=all"
+                entry['docker_options'] = optix_docker_options
                 entry['repository'] = "joaander"
             else:
                 entry['runner'] = "ubuntu-latest"
